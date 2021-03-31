a9285a2038e211b0a05aa95f59cfd37a05ee3bfe
aakashkaler <68342702+aakashkaler@users.noreply.github.com>
2021-03-31 12:12:22 +0530

    Create movezerostoend.java

---
+++ movezerostoend.java
@@ -0,0 +1,23 @@
+public class PushZerosAtEnd {
+	
+	public static void pushZerosAtEnd(int[] arr)
+    {
+		int i=0;
+        int j=0;
+        while(j<arr.length)
+        {
+            if(arr[j]==0)
+            {
+                j++;
+            }
+            else
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j++;
+            }
+        }
+	}
+}
